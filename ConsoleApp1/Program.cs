56348c40c36a18eda79dcf055d6a267a155835d4
adiamante <angelo.c.diamante@gmail.com>
2023-03-29 08:42:02 -0500

    test code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using YantraJS.Core;
+
+var synchronizationContext = new SynchronizationContext();
+var context = new JSContext(synchronizationContext);
+await context.ExecuteAsync("const sleep = ms => new Promise(r => setTimeout(r, ms));");
+await context.ExecuteAsync("await sleep(1000);");
+
+var x = 1;
